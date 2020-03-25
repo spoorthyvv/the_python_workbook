9514e3b2d683ba933d564ab05c2210afdc9f019d
spoorthyvv <spoorthyvv@gmail.com>
2020-03-25 21:14:47 +0530

    Exercise_5

---
+++ introduction_to_programming/exercise_5.py
@@ -0,0 +1,4 @@
+one_litre_container = int(input("Please enter the number of 1 litre containers :"))
+two_litre_container = int(input("Please enter the number of 2 litre containers :"))
+refund = (one_litre_container*0.10)+(two_litre_container*0.25)
+print("Thank you for returning the containers.Your refund amount is $",refund)
