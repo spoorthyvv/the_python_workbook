5e850fd754c2b7bda8e5097618e14172d737eb3a
spoorthyvv <spoorthyvv@gmail.com>
2020-03-25 19:48:22 +0530

    Exercise_4

---
+++ exercise_4.py
@@ -0,0 +1,7 @@
+length = int(input("Enter the length :"))
+#entered in feet
+width = int(input("enter the breadth :"))
+#entered in feet
+area = (length*width)/43560
+#Converting the units from feet to an acre
+print("The area in acres is :",area)
