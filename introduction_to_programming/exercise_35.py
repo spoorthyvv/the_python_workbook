a16546d245d24d851c6187aa12cd184ce58baa51
spoorthyvv <spoorthyvv@gmail.com>
2020-03-30 11:02:30 +0530

    exercise 35

---
+++ introduction_to_programming/exercise_35.py
@@ -0,0 +1,6 @@
+number = float(input("Enter the number to be checked wether even or odd :"))
+if number%2==0:
+	print("The entered number is an even number:")
+else:
+	print("The entered number is an odd number ")
+
